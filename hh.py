13a2ce26012a7af342a8adeedbd8ee1f0d2fb7e4
CapBass <CapBass@mail.ru>
2019-07-28 15:43:01 +1000

    parser hh

--- hh.py
+++ hh.py
@@ -1,23 +1,76 @@
 import requests
-import re
 import csv
 from lxml import html
+import os
 
 headers = {'Accept': '*/*',
            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0'}
 
 
-def get_tree_by_text(text, area=None):
-    params = {'text': text, 'area': area}
+def write_vacancies_to_csv(links, names, salaries, filename='vacancies.csv'):
+    """
+     Function writes data from hh page to csv
+    """
+    data = zip(links, names, salaries)
+    with open(filename, mode='a', newline='', encoding='utf-8') as csv_file:
+        writer = csv.writer(csv_file)
+        if os.stat(filename).st_size == 0:
+            writer.writerow(['Ссылка', 'Наименование', 'Зарплата'])
+        for row in data:
+            writer.writerow(row)
+
+
+def get_vacancies_from_page(page):
+    """
+     Function parses links, link names and salaries from html page and saves it to lists
+    """
+    links = page.xpath('//div[contains(@class, "resume-search-item__name")]//a/@href')
+    names = page.xpath('//div[contains(@class, "resume-search-item__name")]//a/text()')
+    nodes = page.xpath('//div[contains(@class, "vacancy-serp-item__row vacancy-serp-item__row_header")]'
+                       '/div[contains(@class, "vacancy-serp-item__sidebar")]')
+    salaries = []
+    for node in nodes:
+        salary = node.xpath('.//div/text()')
+        if salary:
+            salaries.append(salary[0])
+        else:
+            salaries.append('NULL')
+
+    return links, names, salaries
+
+
+def parse_hh(text, multi_page=True, n=5):
+    """
+     Function parses links, link names and salaries from hh and writes it to csv file
+     text - search parameter (programming language)
+     multi_page - if True, then this function will be parse n pages
+     n - amount of pages
+    """
+    params = {'text': text}
     session = requests.session()
-    request = session.get('https://hh.ru/search/vacancy', headers=headers, params=params)
-    tree = html.fromstring(request.content)
-    size = max(tree.xpath('//a[contains(@class, "bloko-button")]/@data-page'))
-    for i in size[:5]:
-        links = tree.xpath('//div[contains(@class, "resume-search-item__name")]//a/@href')
-        names = tree.xpath('//div[contains(@class, "resume-search-item__name")]//a/text()')
+    try:
+        request = session.get('https://hh.ru/search/vacancy', headers=headers, params=params)
+        page = html.fromstring(request.content)
+        links, names, salaries = get_vacancies_from_page(page)
+        write_vacancies_to_csv(links, names, salaries)
+    except requests.exceptions.ConnectionError:
+        print('Соединение не установлено')
+        exit()
 
-        return links, names
+    if multi_page: # parsing of n pages
+        for i in range(n):
+            url = page.xpath('//a[contains(@class, "bloko-button HH-Pager-Controls-Next HH-Pager-Control")]/@href')[0]
+            url = 'https://hh.ru' + url
+            request = session.get(url, headers=headers)
+            page = html.fromstring(request.content)
+            links, names, salaries = get_vacancies_from_page(page)
+            write_vacancies_to_csv(links, names, salaries)
+        session.close()
+        return None
+    else:
+        session.close()
+        return None
 
 
-print(get_tree_by_text('Python'))
+lang = input('Введите язык программирования ')
+parse_hh(lang)
