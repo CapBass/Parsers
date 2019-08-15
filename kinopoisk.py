e98ad7fd6a5a94db90958e35f279b4450b98ba9c
CapBass <CapBass@mail.ru>
2019-08-15 22:27:46 +1000

    корректировка данных

--- kinopoisk.py
+++ kinopoisk.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 import time
+import re
 
 
 # from pymongo import MongoClient
@@ -13,8 +14,8 @@ import time
 def parse_films(year, main_keys):
     driver = webdriver.Firefox()
     urls = get_urls_films_by_year(driver, year)
-    data = get_data_from_url(driver, urls[0], main_keys)
-    print(data)
+    film_data = correct_film_data(get_data_from_url(driver, urls[0], main_keys))
+    print(film_data)
 
 
 def get_urls_films_by_year(driver, year):
@@ -46,6 +47,17 @@ def get_data_from_url(driver, url, main_keys):
             key = info[i]
         if key in main_keys:
             film_data[key] = value
+    return film_data
+
+
+def correct_film_data(film_data):
+    for key, value in film_data.items():
+        if key == 'время':
+            film_data[key] = int(re.match(r'\d+', value)[0])
+        if '$' in value:
+            film_data[key] = int(re.sub(r'[$ ]', '', value))
+        if key == 'год':
+            film_data[key] = int(value)
     return film_data
 
 
