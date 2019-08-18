ade7b9b8da6e75cf4eea09208ec9de76adf50382
CapBass <CapBass@mail.ru>
2019-08-18 23:39:29 +1000

    kinopoisk

--- kinopoisk.py
+++ kinopoisk.py
@@ -1,24 +1,42 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-import time
 import re
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
 
-
-# from pymongo import MongoClient
-# import pymongo
 
 def parse_films(year, main_keys):
+    """
+    Функция парсит данные с сайта кинопоиск и сохраняет в базу данных mongo
+    :param year: год фильмов
+    :param main_keys: ключи табличной части на странице фильма
+    :return: записывает словарь данных в БД монго
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client['films']
+    films_db = db.films
+    films_db.create_index('film_id', unique=True)
     driver = webdriver.Firefox()
     urls = get_urls_films_by_year(driver, year)
-    film_data = correct_film_data(get_data_from_url(driver, urls[0], main_keys))
-    print(film_data)
+    for url in urls:
+        film_data = correct_film_data(get_data_from_url(driver, url, main_keys))
+        film_data['film_id'] = int(re.findall(r'\d+', url)[0])
+        try:
+            films_db.insert_one(film_data)
+            print(f'Record {film_data["film_id"]} added')
+        except DuplicateKeyError:
+            print(f'Record {film_data["film_id"]} already exist')
+            continue
+        print(film_data)
+    driver.close()
 
 
 def get_urls_films_by_year(driver, year):
+    """
+    Функция получает ссылки на страницы фильмов соответтсвующего года
+    :param driver: вебдрайвер firefox
+    :param year: год фильмов
+    :return: ссылки на страницы фильмов указанного года
+    """
     driver.get(f'https://www.kinopoisk.ru/lists/navigator/{year}/?quick_filters=films')
     assert "КиноПоиск" in driver.title
 
@@ -31,16 +49,29 @@ def get_urls_films_by_year(driver, year):
 
 
 def get_data_from_url(driver, url, main_keys):
+    """
+    Функция получает сырые данных со страницы фильма на кинопоиске
+    :param driver: вебдрайвер firefox
+    :param url: ссылка на страницу фильма
+    :param main_keys: поля табличной части, которые требуется сохранить
+    :return: словарь данных о фильма
+    """
     driver.get(url)
-    # time.sleep(15)
+    # time.sleep(5)
     elem = driver.find_element_by_class_name('moviename-title-wrapper')
     name = elem.text
+    elem = driver.find_element_by_class_name('rating_ball')
+    total_rating = elem.text
+    elem = driver.find_element_by_class_name('ratingCount')
+    total_count = elem.text
     table = driver.find_element_by_xpath('//div[@class="movie-info__content"]//table')
     info = [item.text for item in table.find_elements_by_tag_name('td')]
-    film_data = {'название': name}
+    film_data = {'название': name, 'рейтинг': total_rating, 'кол-во проголосоваваших': total_count}
     value = ''
     key = ''
     for i in range(len(info)):
+        # данные в табличной части хранятся в списке, где
+        # четные элементы - ключи, нечетные - значения
         if i % 2 != 0:
             value = info[i]
         else:
@@ -51,13 +82,31 @@ def get_data_from_url(driver, url, main_keys):
 
 
 def correct_film_data(film_data):
+    """
+    Функция корректрует данные в различных ключах
+    :param film_data: словарь сырых данных о фильме
+    :return: словарь содержащий обработанные данные
+    """
     for key, value in film_data.items():
         if key == 'время':
             film_data[key] = int(re.match(r'\d+', value)[0])
-        if '$' in value:
-            film_data[key] = int(re.sub(r'[$ ]', '', value))
-        if key == 'год':
+        elif '$' in value:
+            value = re.match(r'\d+', re.sub(r'[$ ]', '', value))[0]
             film_data[key] = int(value)
+        elif key == 'рейтинг':
+            film_data[key] = float(value)
+        elif key == 'кол-во проголосоваваших':
+            film_data[key] = int(''.join(value.split(' ')))
+        elif key == 'год':
+            film_data[key] = int(value)
+        elif key in ['сценарий', 'жанр', 'композитор']:
+            values = value.split(',')
+            if '...' in values[-1]:
+                value = ','.join(values[:len(values) - 1])
+                film_data[key] = value
+        else:
+            continue
+
     return film_data
 
 
@@ -66,8 +115,3 @@ MAIN_KEYS = ['год', 'страна', 'режиссер', 'сценарий',
              'композитор', 'жанр', 'бюджет', 'сборы в США',
              'сборы в России', 'время']
 parse_films(YEAR, MAIN_KEYS)
-
-# client = MongoClient('mongodb://127.0.0.1:27017')
-# db = client['mail']
-# mail_db = db.mail_ru
-# mail_db.create_index('letter_id', unique=True)
